24fe3546eadbb0d56baf1f1913f0c4d44a69cbd4
Kenneth Bollen <31512761+kennethbollen@users.noreply.github.com>
2019-01-19 14:32:03 +0100

    Create data_analysis.py

---
+++ data_analysis.py
@@ -0,0 +1,27 @@
+#py3!
+
+import pandas as pd
+import numpy as np
+
+"""creating customer segments"""
+# Define rfm_level function
+def rfm_level(df):
+    if df['RFM_Score'] >= 10:
+        return 'Top'
+    elif ((df['RFM_Score'] >= 6) and (df['RFM_Score'] < 10)):
+        return 'Middle'
+    else:
+        return 'Low'
+
+# Create a new variable RFM_Level
+datamart['RFM_Level'] = datamart.apply(rfm_level, axis=1)
+
+""" analyzing customer segments """
+# Calculate average values for each RFM_Level, and return a size of each segment 
+rfm_level_agg = datamart.groupby('RFM_Level').agg({
+    'Recency': 'mean',
+    'Frequency': 'mean',
+  
+  	# Return the size of each segment
+    'MonetaryValue': ['mean', 'count']
+}).round(1)
