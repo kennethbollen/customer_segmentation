96e87a95c2669ffeadd0087e4e13791673f3e585
Kenneth Bollen <31512761+kennethbollen@users.noreply.github.com>
2019-01-20 18:10:22 +0100

    pre processing for k mean clustering

--- data_analysis.py
+++ data_analysis.py
@@ -2,6 +2,9 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 """creating customer segments"""
 # Define rfm_level function
@@ -25,3 +28,77 @@ rfm_level_agg = datamart.groupby('RFM_Level').agg({
   	# Return the size of each segment
     'MonetaryValue': ['mean', 'count']
 }).round(1)
+
+
+"""pre processing for k-means clustering"""
+
+                            """STEP 1: Determine whether the variables have a similar variance and mean"""
+
+# Print the average values of the variables in the dataset
+print(data.mean())
+
+# Print the standard deviation of the variables in the dataset
+print(data.std())
+
+# Get the key statistics of the dataset
+print(data.describe())
+
+                            """STEP 2: Check the distribution of the variables and their skewness"""
+# Plot distribution of var1
+plt.subplot(3, 1, 1); sns.distplot(data['var1'])
+
+# Plot distribution of var2
+plt.subplot(3, 1, 2); sns.distplot(data['var2'])
+
+# Plot distribution of var3
+plt.subplot(3,1,3); sns.distplot(data['var3'])
+
+# Show the plot
+plt.show()
+
+              """STEP 3: Apply log transformation to the variables that have a skewed distribution (and non-negative values)"""
+
+ # Apply log transformation to var2
+data['var2_log'] = np.log(data['var2'])
+
+# Apply log transformation to var3
+data['var3_log'] = np.log(data['var3'])
+
+# Create a subplot of the distribution of var2_log
+plt.subplot(2, 1, 1); sns.distplot(data['var2_log'])
+
+# Create a subplot of the distribution of var3_log
+plt.subplot(2,1,2); sns.distplot(data['var3_log'])
+# Show the plot
+plt.show()
+
+                                            """STEP 4: Center the variables by scaling"""
+#Option 1 scaling is to manually center
+
+# Center the data by subtracting average values from each entry
+data_centered = data - data.mean()
+
+# Scale the data by dividing each entry by standard deviation
+data_scaled = data / data.std()
+
+# Normalize the data by applying both centering and scaling
+data_normalized = (data - data.mean()) / data.std()
+
+# Print summary statistics to make sure average is zero and standard deviation is one
+print(data_normalized.describe().round(2))
+
+#Option 2 scaling is to use the preprocessing package from sklearn
+# Initialize a scaler
+scaler = StandardScaler()
+
+# Fit the scaler
+scaler.fit(data)
+
+# Scale and center the data
+data_normalized = scaler.transform(data)
+
+# Create a pandas DataFrame
+data_normalized = pd.DataFrame(data_normalized, index=data.index, columns=data.columns)
+
+# Print summary statistics
+print(data_normalized.describe().round(2))
